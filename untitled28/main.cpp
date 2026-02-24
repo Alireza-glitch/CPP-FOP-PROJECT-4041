5d701d8d57f18f573865c438144f344cdf3a1b7d
Armin2048 <armindehghan832@gmail.com>
2026-02-25 01:43:07 +0330

    blucks

--- untitled28/main.cpp
+++ untitled28/main.cpp
@@ -4,7 +4,6 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,18 +14,198 @@
 #include <cstdarg>
 #include <cstdio>
 #include <algorithm>
-
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
 using namespace std;
-
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 #define MAX_STEPS_PER_FRAME 10000
 #define SPRITE_EDIT_WIDTH 180
 #define SPRITE_EDIT_HEIGHT 120
+SDL_Window* gWindow = NULL;
+struct Application* gApp = nullptr;
+//Value System
+struct Value {
+    enum Type { VAL_NUMBER, VAL_STRING } type;
+    float num;
+    string str;
 
+    Value() : type(VAL_NUMBER), num(0) {}
+};
+
+Value make_number(float f) {
+    Value v;
+    v.type = Value::VAL_NUMBER;
+    v.num = f;
+    return v;
+}
+
+Value make_string(const string& s) {
+    Value v;
+    v.type = Value::VAL_STRING;
+    v.str = s;
+    return v;
+}
+
+float value_to_number(const Value& v) {
+    if (v.type == Value::VAL_NUMBER) return v.num;
+    return (float)atof(v.str.c_str());
+}
+
+string value_to_string(const Value& v) {
+    if (v.type == Value::VAL_STRING) return v.str;
+    return to_string(v.num);
+}
+
+// Forward declarations
+struct Application;
+struct Project;
+struct ExecutionEngine;
+struct SpriteManagerUI;
+struct BackdropManagerUI;
+struct SoundManagerUI;
+struct PenToolUI;
+struct BlockPaletteUI;
+struct CodeAreaUI;
+struct Sprite;
+struct Backdrop;
+struct Sound;
+struct Variable;
+struct ExecutionContext;
+struct Block;
+struct Script;
+struct Costume;
+
+struct LoopInfo {
+    int start;
+    int end;
+    int count;
+};
+
+struct IfInfo {
+    int elseStart;
+    int endifPos;
+    bool trueBranch;
+};
+
+enum BlockType {
+    BLOCK_MOVE,
+    BLOCK_TURN,
+    BLOCK_GOTO,
+    BLOCK_CHANGE_X,
+    BLOCK_CHANGE_Y,
+    BLOCK_SET_DIRECTION,
+    BLOCK_SAY,
+    BLOCK_THINK,
+    BLOCK_SWITCH_COSTUME,
+    BLOCK_NEXT_COSTUME,
+    BLOCK_SWITCH_BACKDROP,
+    BLOCK_NEXT_BACKDROP,
+    BLOCK_CHANGE_SIZE,
+    BLOCK_SET_SIZE,
+    BLOCK_CHANGE_COLOR,
+    BLOCK_SET_COLOR,
+    BLOCK_CLEAR_EFFECTS,
+    BLOCK_SHOW,
+    BLOCK_HIDE,
+    BLOCK_GO_TO_LAYER,
+    BLOCK_CHANGE_LAYER,
+    BLOCK_PLAY_SOUND,
+    BLOCK_PLAY_SOUND_UNTIL_DONE,
+    BLOCK_STOP_ALL_SOUNDS,
+    BLOCK_CHANGE_VOLUME,
+    BLOCK_SET_VOLUME,
+    BLOCK_WHEN_FLAG_CLICKED,
+    BLOCK_WHEN_KEY_PRESSED,
+    BLOCK_WHEN_SPRITE_CLICKED,
+    BLOCK_BROADCAST,
+    BLOCK_BROADCAST_AND_WAIT,
+    BLOCK_WHEN_I_RECEIVE,
+    BLOCK_WAIT,
+    BLOCK_REPEAT,
+    BLOCK_FOREVER,
+    BLOCK_IF,
+    BLOCK_IF_ELSE,
+    BLOCK_WAIT_UNTIL,
+    BLOCK_REPEAT_UNTIL,
+    BLOCK_STOP_ALL,
+    BLOCK_CUSTOM_CALL,
+    BLOCK_CUSTOM_DEFINE,
+    BLOCK_ADD,
+    BLOCK_SUBTRACT,
+    BLOCK_MULTIPLY,
+    BLOCK_DIVIDE,
+    BLOCK_RANDOM,
+    BLOCK_LT,
+    BLOCK_GT,
+    BLOCK_EQUALS,
+    BLOCK_AND,
+    BLOCK_OR,
+    BLOCK_NOT,
+    BLOCK_JOIN,
+    BLOCK_LETTER_OF,
+    BLOCK_LENGTH,
+    BLOCK_MOD,
+    BLOCK_ROUND,
+    BLOCK_ABS,
+    BLOCK_SQRT,
+    BLOCK_SIN,
+    BLOCK_COS,
+    BLOCK_TAN,
+    BLOCK_ASIN,
+    BLOCK_ACOS,
+    BLOCK_ATAN,
+    BLOCK_LN,
+    BLOCK_LOG,
+    BLOCK_POW,
+    BLOCK_SET_VARIABLE,
+    BLOCK_CHANGE_VARIABLE,
+    BLOCK_VARIABLE_GET,
+    BLOCK_NUMBER,
+    BLOCK_STRING,
+    BLOCK_TOUCHING_EDGE,
+    BLOCK_MOUSE_X,
+    BLOCK_MOUSE_Y,
+    BLOCK_KEY_PRESSED,
+    BLOCK_PEN_DOWN,
+    BLOCK_PEN_UP,
+    BLOCK_SET_PEN_COLOR,
+    BLOCK_CHANGE_PEN_COLOR,
+    BLOCK_SET_PEN_BRIGHTNESS,
+    BLOCK_CHANGE_PEN_BRIGHTNESS,
+    BLOCK_SET_PEN_SATURATION,
+    BLOCK_CHANGE_PEN_SATURATION,
+    BLOCK_SET_PEN_SIZE,
+    BLOCK_CHANGE_PEN_SIZE,
+    BLOCK_ERASE_ALL,
+    BLOCK_STAMP,
+    BLOCK_CHANGE_BRIGHTNESS,
+    BLOCK_SET_BRIGHTNESS,
+    BLOCK_CHANGE_SATURATION,
+    BLOCK_SET_SATURATION,
+    BLOCK_COSTUME_NUMBER,
+    BLOCK_COSTUME_NAME,
+    BLOCK_BACKDROP_NUMBER,
+    BLOCK_BACKDROP_NAME,
+    BLOCK_SIZE,
+    BLOCK_TOUCHING_MOUSEPOINTER,
+    BLOCK_TOUCHING_SPRITE,
+    BLOCK_TOUCHING_COLOR,
+    BLOCK_COLOR_TOUCHING_COLOR,
+    BLOCK_DISTANCE_TO,
+    BLOCK_ASK_AND_WAIT,
+    BLOCK_ANSWER,
+    BLOCK_MOUSE_DOWN,
+    BLOCK_SET_DRAG_MODE,
+    BLOCK_TIMER,
+    BLOCK_RESET_TIMER,
+    BLOCK_GO_TO_RANDOM,
+    BLOCK_GO_TO_MOUSE,
+    BLOCK_IF_ON_EDGE_BOUNCE,
+    BLOCK_ELSE,
+    BLOCK_ENDIF,
+    BLOCK_ENDLOOP
+};
 
