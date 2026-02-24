fbce198e56c88c0460d961e4c927e4842060960f
Alireza-glitch <alireza.frj1386@gmail.com>
2026-02-25 01:31:34 +0330

    headers

--- untitled28/main.cpp
+++ untitled28/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#define _USE_MATH_DEFINES
+#define _GNU_SOURCE
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstdio>
+#include <algorithm>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+using namespace std;
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
